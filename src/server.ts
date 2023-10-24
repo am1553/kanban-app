a56253ebe2129810afed08856fd4631a36c545bb
Aryan Mehta <personal@Aryans-MacBook-Pro.local>
2023-10-24 22:44:20 +0100

    added credentirals to cors

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import cors from "cors";
 import { protect } from "./modules/auth";
 import { createUser, signin } from "./handlers/users";
 const app = express();
-app.use(cors());
+app.use(cors({ credentials: true }));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use("/api/v1", protect, router);
